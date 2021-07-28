5179a72d728e0fbc4d7ab4f669b3c205df020e22
John Nunemaker <nunemaker@gmail.com>
2021-07-28 16:42:43 -0400

    Add more examples to comparison

--- examples/comparison.rb
+++ examples/comparison.rb
@@ -1,23 +1,42 @@
 require 'bundler/setup'
 require 'flipper'
 
-basic_actor = Flipper::Actor.new(1, {
+basic_39_actor = Flipper::Actor.new(1, {
   "plan" => "basic",
+  "age" => 39,
 })
-premium_actor = Flipper::Actor.new(2, {
+premium_40_actor = Flipper::Actor.new(2, {
   "plan" => "premium",
+  "age" => 40,
 })
 no_plan_actor = Flipper::Actor.new(3)
 
-p basic: Flipper.enabled?(:redesign, basic_actor)
-p premium: Flipper.enabled?(:redesign, premium_actor)
-p no_plan: Flipper.enabled?(:redesign, no_plan_actor)
+p({
+  basic_39: Flipper.enabled?(:redesign, basic_39_actor),
+  premium_40: Flipper.enabled?(:redesign, premium_40_actor),
+  no_plan: Flipper.enabled?(:redesign, no_plan_actor),
+})
 
 puts "Enabling for basic"
 Flipper.enable_comparison :redesign, "plan", "eq", "basic"
-# Or... Flipper.enable :redesign, Flipper::Types::Comparison.new(["plan", "eq", "basic"])
-# Or... Flipper.enable_comparison :redesign, ["plan", "eq", "basic"]
+p({
+  basic_39: Flipper.enabled?(:redesign, basic_39_actor),
+  premium_40: Flipper.enabled?(:redesign, premium_40_actor),
+  no_plan: Flipper.enabled?(:redesign, no_plan_actor),
+})
 
-p basic: Flipper.enabled?(:redesign, basic_actor)
-p premium: Flipper.enabled?(:redesign, premium_actor)
-p no_plan: Flipper.enabled?(:redesign, no_plan_actor)
+Flipper.disable_comparison :redesign, "plan", "eq", "basic"
+puts "Disabling for basic"
+p({
+  basic_39: Flipper.enabled?(:redesign, basic_39_actor),
+  premium_40: Flipper.enabled?(:redesign, premium_40_actor),
+  no_plan: Flipper.enabled?(:redesign, no_plan_actor),
+})
+
+puts "Enabling for >= 40"
+Flipper.enable_comparison :redesign, "age", "gte", 40
+p({
+  basic_39: Flipper.enabled?(:redesign, basic_39_actor),
+  premium_40: Flipper.enabled?(:redesign, premium_40_actor),
+  no_plan: Flipper.enabled?(:redesign, no_plan_actor),
+})
